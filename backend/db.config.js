b1cc98f3424a49dc538f145313075a2ca1fb1448
Piero <piero.neri@hotmail.fr>
2023-03-23 09:27:31 +0100

    initial commit

---
+++ backend/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  HOST: "TO_MODIFY",
+  USER: "TO_MODIFY",
+  PASSWORD: "TO_MODIFY",
+  DATABASE: "TO_MODIFY",
+  PORT: "TO_MODIFY",
+};
---
+++ frontend/src/API.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const getTodos = async () => {
+  //TO_MODIFY
+};
+
+export const createTodo = async (formData) => {
+  //TO_MODIFY
+};
+
+export const deleteTodo = async (todoId) => {
+  //TO_MODIFY
+};
