270033566573ba2a904af133da87c40665bdcbe0
Piero <piero.neri@hotmail.fr>
2023-03-23 11:24:26 +0100

    question 5

--- backend/db.config.js
+++ backend/db.config.js
@@ -1,7 +1,8 @@
+
 module.exports = {
-  HOST: "db",
-  USER: "user",
-  PASSWORD: "password",
-  DATABASE: "app",
-  PORT: "3306",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DATABASE: process.env.DB_NAME,
+  PORT: process.env.DB_PORT,
 };
